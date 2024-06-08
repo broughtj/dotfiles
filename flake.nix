5448e8ddd60bea28de31a18d5f94a69e5e4bdda0
Tyler Brough <broughtj@gmail.com>
2024-06-08 14:53:15 -0600

    I don't know wtf about anything!

--- flake.nix
+++ flake.nix
@@ -1,6 +1,61 @@
 {
   description = "Example Darwin system flake";
 
+  # There are sets, except their definition of a set is wrong and bad. They really
+  # mean a dictionary or map (sets of key-value pairs).
+  #
+  # When they write:
+  # inputs.nixpkgs.url = 1;
+  # it is shorthand for
+  # { inputs = { nixpkgs = { url = 1; }}}.
+  #
+  # There are also functions. They look like this:
+  #
+  # x: x + 2.
+  #
+  # Most of the time though, the functions will take sets as inputs, and they 
+  # use pattern-matching (or destructuring in Python lingo) syntax to destructure
+  # the set arguments. For example,
+  #
+  # { self, nix-darwin, nixpkgs }: self.foo
+  #
+  # would take a set with the keys `self`, `nix-darwin`, `nixpkgs` and then 
+  # return the value of `foo` in the dictionary `self.`
+  #
+  # Functional programmers don't like parenthesis, so to call a function f on an input x looks like
+  #
+  # f x
+  #
+  # Parenthesis are instead used to indicate calling order when expresssions are complex. For example
+  #
+  # f (g x)
+  #
+  # is notation for first calling g on x and then calling f on the result.
+  #
+  # There's some syntax for `let` bindings, and there's a fancy thing for `with`,
+  # but other than that, that's basically the whole thing.
+  #
+  # There is also the inherit syntax, which just helps reduce boilerplate. The syntax
+  #
+  # { inherit foo; }
+  #
+  # is just sugar for
+  #
+  # { foo = foo; }
+  #
+  # The whole point of this is that we functionally describe package dependencies
+  # without any side-effects. No implicit dependencies is the whole goal.
+  #
+  # TODO:
+  # - Nix modules (https://nixos.wiki/wiki/NixOS_modules)
+  #
+  # Articles:
+  #
+  # Article describing flakes and specifically the output schema:
+ # https://nixos.wiki/wiki/Flakes#Flake_schema
+  #
+  #
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -9,39 +64,118 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nix-darwin, home-manager }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, emacs-overlay }:
     let
-      system = "x86_64-darwin"; # or "aarch64-darwin" if you are on an M1 Mac
-      pkgs = import nixpkgs { inherit system; };
-      home = home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs;
-        modules = [ ./home.nix ];
-      };
-    in {
-      nixosModules = {
-        frisell = { ... }: {
-          imports = [ home ];
+      emacsOverlay = (pkgs: package: 
+        (pkgs.emacsWithPackagesFromUsePackage {
+          inherit package;
+          config = ./emacs.el;
+          defaultInitFile = true;
+          alwaysEnsure = true;
+        })
+      );
+    in
+    rec {
+      nixosModules = rec {
+        darwinSystem = { pkgs, ... }: {
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 4;
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+          nix.package = pkgs.nixFlakes;
 
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [ pkgs.neovim ];
 
-          home-manager.enable = true;
+          # programs.zsh = {
+	    # enable = true;
+            # enableAutosuggestions = true;
+          # }
+	 
+          programs.fish.enable = true;
+	  #programs.zsh.enable = false;
+	  #programs.nushell.enable = true;
+
+
+          homebrew.enable = true;
+          homebrew.casks = [
+            "dropbox"
+            "fantastical"
+            "google-chrome"
+            "google-drive"
+            "mathpix-snipping-tool"
+	    "microsoft-teams"
+	    "obsidian"
+            "slack"
+            "spotify"
+            "todoist"
+            "visual-studio-code"
+            "vlc"
+            "warp"
+            "zoom"
+            "zotero"
+          ];
+        };
+        frisell = { pkgs, ... }: {
+          imports = [ darwinSystem ];
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          # This is TJB now rather than autogen stuff
+          users.users.tjb = {
+            home = "/Users/tjb/";
+	    #shell = pkgs.zsh;
+            shell = pkgs.fish;
+	    #shell = pkgs.nushell;
+          };
+        };
+        cooder = { pkgs, ...}: {
+          imports = [ darwinSystem ];
+
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          users.users.tylerbrough = {
+            home = "/Users/tylerbrough";
+	    #shell = pkgs.zsh;
+            shell = pkgs.fish;
+	    #shell = pkgs.nushell;
+          };
+        };
+        home = { config, pkgs, ... }: {
+          imports = [ emacsConfiguration ];
+
+          # No touchy, seriously don't ever change this
+          home.stateVersion = "23.11";
+          programs.home-manager.enable = true;
 
           home.homeDirectory = "/Users/${config.home.username}";
 
           home.packages = with pkgs; [
-            djvu2pdf
+	    djvu2pdf
             direnv
-            nushell
-            pandoc
+	    ghostscript
+	    imagemagick
+	    nushell
+	    pandoc
             ripgrep
-            tree
-            imagemagick # Added imagemagick here
+            tree 
           ];
 
-          programs.fish.enable = true;
+          #programs.zsh.enable = true;
+	  programs.fish.enable = true;
+	  #programs.nushell.enable = true;
 
           programs.git = {
             enable = true;
@@ -69,6 +203,8 @@
           programs.emacs.enable = true;
         };
       };
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
       darwinConfigurations."Frisell" = nix-darwin.lib.darwinSystem {
         modules = [ nixosModules.frisell ];
       };
