d84a5bd6bf155fdf1cdbc24bcac98d8fe44d0316
Tyler Brough <broughtj@gmail.com>
2024-12-03 13:34:16 -0700

    Again.

--- flake.nix
+++ flake.nix
@@ -2,23 +2,19 @@
   description = "Dotfiles configuration with nix-darwin and Home Manager for macOS (Apple Silicon)";
 
   inputs = {
-    # Nixpkgs for packages
     nixpkgs.url = "github:NixOS/nixpkgs";
 
-    # Home Manager
     home-manager.url = "github:nix-community/home-manager";
 
-    # nix-darwin
     nix-darwin.url = "github:LnL7/nix-darwin";
   };
 
   outputs = { self, nixpkgs, home-manager, nix-darwin }: {
     darwinConfigurations = {
-      # Replace 'Hostname' with your macOS hostname
-      Hostname = nix-darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
+      Frisell = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin"; # Apple Silicon architecture
         modules = [
-          ./darwin-configuration.nix # Nix-darwin configuration file
+          ./darwin-configuration.nix
           home-manager.nixosModules.home-manager
         ];
         specialArgs = { inherit home-manager; };
@@ -26,13 +22,12 @@
     };
 
     homeConfigurations = {
-      # Replace 'tjb@Hostname' with your desired identifier
-      "tjb@Hostname" = home-manager.lib.homeManagerConfiguration {
+      "tjb@Frisell" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "aarch64-darwin";
         };
         modules = [
-          ./home.nix # Home Manager configuration file
+          ./home.nix
         ];
       };
     };
